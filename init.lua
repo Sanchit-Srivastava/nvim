49511010180234d89af45bfb72fde3c0a9553cbf
Sanchit Srivastava <mail2sanchit97@gmail.com>
2024-11-23 22:44:44 -0500

    cleaned up lua.init as per ep2

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader= " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -17,39 +11,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
-local plugins = {
-   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
-  "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
-    -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
-  end
- }
-}
-require("lazy").setup(plugins, opts)
+--local opts = {}
+require("lazy").setup("plugins")
+require("vim-options")
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript", "python", "latex"},
-  highlight = { enable = true },
-  indent = { enable = true }
-})
 
---require catppuccin
-require("catppuccin").setup()
-
---set the colorscheme to it!
-vim.cmd.colorscheme "catppuccin"
