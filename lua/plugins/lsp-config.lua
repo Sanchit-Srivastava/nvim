000d84f742672ee2ffba18116ee6a3c29c353a10
Sanchit Srivastava <mail2sanchit97@gmail.com>
2024-11-24 02:27:14 -0500

    done with lsp for now

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,27 +1,29 @@
 return {
   {
-    "williamboman/mason.nvim", 
+    "williamboman/mason.nvim",
     config = function()
     require('mason').setup()
     end
   },
   {
-    "williamboman/mason-lspconfig.nvim" , 
+    "williamboman/mason-lspconfig.nvim" ,
     config = function()
       require('mason-lspconfig').setup({
         ensure_installed = { "lua_ls","texlab", "grammarly", "pyright"}
       })
     end
-  },   
+  },
   {
-    "neovim/nvim-lspconfig", 
-    config = function() 
+      "neovim/nvim-lspconfig",
+      config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.texlab.setup({})
       lspconfig.grammarly.setup({})
       lspconfig.pyright.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'},'<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
