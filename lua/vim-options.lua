bc25563e328504210e9f4b22290d869a7aaf36c2
Sanchit Srivastava <mail2sanchit97@gmail.com>
2024-11-25 14:05:27 -0500

    added completions and md

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,5 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-
+vim.cmd("filetype plugin on")
+vim.cmd("set encoding=utf-8")
+vim.g.vimtex_quickfix_mode = 0
+vim.g.vimtex_log_verbose = 0
 
