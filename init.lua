ff8fe903b4f42fbb957b3591551962cb5cd1b02a
Sanchit Srivastava <mail2sanchit97@gmail.com>
2024-11-25 17:04:54 -0500

    updated and cleaned up vim options

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-vim.g.mapleader= " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -12,8 +11,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
 require("vim-options")
+require("lazy").setup("plugins")
 
 
 
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,9 +1,41 @@
+--Leader is space key
+vim.g.mapleader = " "
+
+--Line numbering : using hybrid for now
+vim.opt.nu = true
+vim.opt.rnu = true
+
+-- Indenting
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("filetype plugin on")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+--backup and undo
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("Home") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+--vim search options
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+
+--colourse
+vim.opt.termguicolors = true
+
+--scrollingc
+vim.opt.scrolloff = 8
+
+--random
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
+--Vimtex options
 vim.cmd("set encoding=utf-8")
+vim.cmd("filetype plugin on")
 vim.g.vimtex_quickfix_mode = 0
 vim.g.vimtex_log_verbose = 0
-
